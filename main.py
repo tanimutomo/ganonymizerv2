51be3eb613aaad25f570d217ee608e0037b29cf8
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-02-27 21:40:21 +0900

    add deeplabv3's semantic segmentation to GANonymizer model

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import torch
+
+from modules.model import GANonymizer
+from modules.deeplabv3.model.deeplabv3 import DeepLabV3
+from modules.utils import Debugger, set_networks
+
+
+def main(img, params, debug=False, save=False):
+    # setup environment
+    debugger = Debugger(debug, save)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # setup models
+    semseger = set_networks(DeepLabV3, params['resnet'], device)
+    inpainter = None
+    shadow_detecter = None
+
+    # model prediction
+    model = GANonymizer(params, device, semseger, inpainter, shadow_detecter, debugger)
+    model.predict(img)
+
+
+if __name__ == '__main__':
+    img = os.path.join(os.getcwd(), 'data/examples/example_01.jpg')
+    params = {
+            'resnet': 18
+            }
+
+    main(img, params, debug=False, save=False)
+
