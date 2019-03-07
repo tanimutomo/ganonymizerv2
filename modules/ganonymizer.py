f941de91e4ef3f9c139f503062c27502ad4565dc
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-03-07 23:31:56 +0900

    separate mask and shadow detection

--- modules/ganonymizer.py
+++ modules/ganonymizer.py
@@ -27,7 +27,7 @@ class GANonymizer:
     
     def predict(self, img_path):
         # Loading input image
-        print('----- Loading Image -----')
+        print('\n===== Loading Image =====')
         fname, fext = img_path.split('/')[-1].split('.')
         img = Image.open(img_path)
         img = img.resize((int(img.width / 4), int(img.height / 4)))
@@ -38,7 +38,7 @@ class GANonymizer:
         self.debugger.img(img, 'Input Image')
 
         # semantic segmentation
-        print('----- Semantic Segmentation -----')
+        print('\n===== Semantic Segmentation =====')
 
         segmap_path = os.path.join(self.config['checkpoint'], fname + '_segmap' + 'pkl')
         if self.config['semseg_mode'] is 'exec':
@@ -56,20 +56,20 @@ class GANonymizer:
         self.debugger.img(semseg_map, 'Semantic Segmentation Map Prediction by DeepLabV3')
 
 
-        # shadow detection
-        print('----- Shadow Detection and Creating Mask Image -----')
+        # create mask image and image with mask
+        print('\n===== Creating Mask Image =====')
         mask_path = os.path.join(self.config['checkpoint'], fname + '_mask' + 'pkl')
         img_with_mask_path = os.path.join(self.config['checkpoint'], fname + '_img_with_mask' + 'pkl')
 
-        if self.config['shadow_mode'] is 'exec':
-            mask, img_with_mask = self.shadow_detecter.detect(img, semseg_map, self.labels)
-        elif self.config['shadow_mode'] is 'pass':
+        if self.config['mask_mode'] is 'exec':
+            mask, img_with_mask = self.shadow_detecter.mask(img, semseg_map, self.labels)
+        elif self.config['mask_mode'] is 'pass':
             with open(mask_path, mode='rb') as f:
                 mask = pickle.load(f)
             with open(img_with_mask_path, mode='rb') as f:
                 img_with_mask = pickle.load(f)
-        elif self.config['shadow_mode'] is 'save':
-            mask, img_with_mask = self.shadow_detecter.detect(img, semseg_map, self.labels)
+        elif self.config['mask_mode'] is 'save':
+            mask, img_with_mask = self.shadow_detecter.mask(img, semseg_map, self.labels)
             with open(mask_path, mode='wb') as f:
                 pickle.dump(mask, f)
             with open(img_with_mask_path, mode='wb') as f:
@@ -82,8 +82,13 @@ class GANonymizer:
         self.debugger.img(img_with_mask, 'img_with_mask')
 
 
+        # shadow detection
+        print('\n===== Shadow Detection =====')
+        self.shadow_detecter.detect(img, mask, img_with_mask)
+
+
         # inpainter
-        print('----- Image Inpainting and Edge Inpainting -----')
+        print('\n===== Image Inpainting and Edge Inpainting =====')
         inpaint_path = os.path.join(self.config['checkpoint'], fname + '_inpaint' + 'pkl')
         inpaint_edge_path = os.path.join(self.config['checkpoint'], fname + '_inpaint_edge' + 'pkl')
 
