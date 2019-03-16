9c69752dc031a9efe5e2a353dc95902f3d70ee9e
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-03-16 22:39:00 +0900

    get resized inpainted images

--- modules/mask_divider.py
+++ modules/mask_divider.py
@@ -28,6 +28,9 @@ class MaskDivider:
         # get large object
         large_objects = self._large_object(obj_labelmap)
 
+        # apply inpainting to resized image for each large object
+        large_objects = self._get_sml_inpainted(large_objects, img, mask)
+
 
     def _separate_objects(self, mask):
         # separate object in the mask using random walker algorithm
@@ -143,5 +146,54 @@ class MaskDivider:
 
         self.debugger2.param(larges, 'larges')
         return larges
+
+
+    def _get_sml_inpainted(self, large_objects, img, mask):
+        H, W = img.shape[0], img.shape[1]
+
+        # apply inpaint each resized image
+        for obj in large_objects:
+            resimg, resmask = img.copy(), mask.copy()
+
+            # calcurate new height and width
+            height, width = obj['height'], obj['width']
+            base = max(width, height)
+            ratio = self.config.obj_wh_thresh / base
+            new_h, new_w = int(H * ratio), int(W * ratio)
+            new_h = new_h if new_h % 4 == 0 else new_h - (new_h % 4)
+            new_w = new_w if new_w % 4 == 0 else new_w - (new_w % 4)
+            self.debugger3.param(ratio, 'ratio')
+            self.debugger3.param(new_w, 'new w')
+            self.debugger3.param(new_h, 'new h')
+
+            # resize inputs
+            resimg = cv2.resize(resimg, (new_w, new_h)).astype(np.uint8)
+            resmask = cv2.resize(resmask, (new_w, new_h)).astype(np.uint8)
+            resmask = np.where(resmask > 127, 255, 0)
+            self.debugger3.img(resimg, 'resized image')
+            self.debugger3.img(resmask, 'resized mask')
+
+            # apply inpainting
+            inpainted, inpainted_edge, edge = self.inpainter.inpaint(resimg, resmask) 
+            self.debugger3.img(inpainted, 'inpainted')
+            self.debugger3.img(inpainted_edge, 'inpainted_edge')
+            self.debugger3.img(edge, 'edge')
+
+            # resize output to original size
+            inpainted = cv2.resize(inpainted, (W, H))
+            inpainted_edge = cv2.resize(inpainted_edge, (W, H))
+            edge = cv2.resize(edge, (W, H))
+            self.debugger3.img(inpainted, 'inpainted')
+            self.debugger3.img(inpainted_edge, 'inpainted_edge')
+            self.debugger3.img(edge, 'edge')
+
+            # add output to objects dict
+            obj['inpainted'] = inpainted
+            obj['inpainted_edge'] = inpainted_edge
+            obj['edge'] = edge
+
+        self.debugger3.param(large_objects, 'large objects')
+        return large_objects
+
 
 
