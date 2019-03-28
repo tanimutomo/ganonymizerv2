927d495907a343dce74acc95a278077fb9ce773c
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-03-28 10:48:36 +0900

    add rectangle option

--- modules/randmask_creater.py
+++ modules/randmask_creater.py
@@ -8,17 +8,21 @@ class RandMaskCreater:
     def __init__(self, config):
         self.config = config
         self.debugger = Debugger(config.random_mode, save_dir=config.checkpoint)
-        self.min_size = config.rmask_min
-        self.max_size = config.rmask_max
     
     def sample(self, mask):
         mask = np.where(mask > 0, 1, 0).astype(np.uint8)
         MAX_ITER = 100
         for idx in range(MAX_ITER):
             rmask = np.zeros_like(mask)
-            shortd, longd, angle, cx, cy = self._sample_ellipse_coord(mask)
             self.debugger.matrix(rmask, 'rmask')
-            rmask = cv2.ellipse(rmask, ((cx, cy), (longd, shortd), angle), 1, thickness=-1)
+            # ellipse mask
+            if self.config.rmask_shape == 'ellipse':
+                shortd, longd, angle, cx, cy = self._sample_ellipse_coord(mask)
+                rmask = cv2.ellipse(rmask, ((cx, cy), (longd, shortd), angle), 1, thickness=-1)
+            # rectangle mask
+            elif self.config.rmask_shape == 'rectangle':
+                tl, br = self._sample_rectangle_coord(self, mask)
+                rmask = cv2.rectangle(rmask, tl, br, 1, -1)
             self.debugger.matrix(mask, 'mask')
             self.debugger.matrix(rmask, 'rmask')
             if not np.any(mask + rmask > 1):
@@ -29,14 +33,26 @@ class RandMaskCreater:
         return rmask.astype(np.uint8) * 255
 
     def _sample_ellipse_coord(self, mask):
+        min_diameter = int(self.config.min_size / 2)
+        max_diameter = int(self.config.max_size / 2)
         while True:
-            short_diameter = random.randint(self.min_size, self.max_size)
-            long_diameter = random.randint(self.min_size, self.max_size)
+            short_diameter = random.randint(min_diameter, max_diameter)
+            long_diameter = random.randint(min_diameter, max_diameter)
             if short_diameter < long_diameter:
                 break
         angle = random.randint(0, 180)
-        cx = random.randint(self.max_size, mask.shape[1] - self.max_size)
-        cy = random.randint(self.max_size, mask.shape[0] - self.max_size)
+        cx = random.randint(max_diameter, mask.shape[1] - max_diameter)
+        cy = random.randint(max_diameter, mask.shape[0] - max_diameter)
         return short_diameter, long_diameter, angle, cx, cy
+
+    def _sample_rectangle_coord(self, mask):
+        side_len = random.randint(self.config.min_size, self.config.max_size)
+        cx = random.randint(self.config.max_size, mask.shape[1] - self.config.max_size)
+        cy = random.randint(self.config.max_size, mask.shape[0] - self.config.max_size)
+        half_len = int(side_len / 2)
+        tl = (cx - half_len, cy - half_len)
+        br = (cx + half_len, cy + half_len)
+        return tl, br
+
 
 
