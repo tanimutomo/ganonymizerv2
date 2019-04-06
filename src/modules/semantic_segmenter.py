480b279615f4fee09b66ad239f9c207535eb1c63
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-04-06 19:06:07 +0900

    use module root for weight path

--- src/modules/semantic_segmenter.py
+++ src/modules/semantic_segmenter.py
@@ -5,15 +5,14 @@ from .utils import Debugger
 
 class SemanticSegmenter():
     def __init__(self, config, device):
-        self.network = config.semseg
-        self.resnet = config.resnet
+        self.config = config
         self.device = device
         self.debugger = Debugger(config.semseg_mode, save_dir=config.checkpoint)
 
-        if self.network == 'DeepLabV3':
+        if self.config.semseg == 'DeepLabV3':
             self.model = self._set_deeplabv3()
         else:
-            raise RuntimeError('Please prepare {} model in modules/ .'.format(self.network))
+            raise RuntimeError('Please prepare {} model in modules/ .'.format(self.config.semseg))
 
 
     def predict(self, img):
@@ -24,11 +23,11 @@ class SemanticSegmenter():
 
 
     def _set_deeplabv3(self):
-        weights = os.path.join(os.getcwd(),
-                'modules/deeplabv3/pretrained/model_13_2_2_2_epoch_580.pth')
-        resnet_root = os.path.join(os.getcwd(),
-            'modules/deeplabv3/pretrained/resnet')
-        model = DeepLabV3(self.resnet, resnet_root, self.device)
+        weights = os.path.join(self.config.mod_root,
+                'deeplabv3/pretrained/model_13_2_2_2_epoch_580.pth')
+        resnet_root = os.path.join(self.config.mod_root,
+                'deeplabv3/pretrained/resnet')
+        model = DeepLabV3(self.config.resnet, resnet_root, self.device)
         param = torch.load(weights, map_location=self.device)
         model.load_state_dict(param)
         model.eval() # (set in evaluation mode, this affects BatchNorm and dropout)
